31d0a115152130b56b5029504f218556a323eacd
rreda11 <141592396+rreda11@users.noreply.github.com>
2025-06-08 22:37:29 +0300

    Delete Django-Middleware-0x03/Django-Middleware-0x03/chats directory

--- Django-Middleware-0x03/Django-Middleware-0x03/chats/auth.py
+++
@@ -1,7 +0,0 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-TokenObtainPairView = TokenObtainPairView.as_view()
-TokenRefreshView = TokenRefreshView.as_view()
