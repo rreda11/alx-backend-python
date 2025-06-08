b0fb25e0215aed01e4f33e2c4f0a6de915674f1c
rreda11 <141592396+rreda11@users.noreply.github.com>
2025-06-08 21:56:00 +0300

    Add files via upload

---
+++ messaging_app/chats/auth.py
@@ -0,0 +1,7 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+TokenObtainPairView = TokenObtainPairView.as_view()
+TokenRefreshView = TokenRefreshView.as_view()
